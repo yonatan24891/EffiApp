51695d183623ff522da9891dca34efef9c07db2d
yonatan24891 <yonatan24891@hotmail.com>
2016-08-29 21:15:40 +0100

    Informacion detallada acabada

--- app/src/main/java/yonatan24891/effiapp/EvaluarActivity.java
+++ app/src/main/java/yonatan24891/effiapp/EvaluarActivity.java
@@ -1,26 +1,46 @@
 package yonatan24891.effiapp;
 
 import android.app.ActivityManager;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Debug;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+
+
 public class EvaluarActivity extends AppCompatActivity {
+
+    public void goToEvaluar(View view) {
+        Intent intent = new Intent(this, EvaluarActivity.class);
+        startActivity(intent);
+    }
 
     double DistanciaMinima(double x, ResourceData ABCD) {
 
@@ -47,20 +67,20 @@ public class EvaluarActivity extends AppCompatActivity {
         ActivityManager am = (ActivityManager) this.getSystemService(ACTIVITY_SERVICE);
         // The first in the list of RunningTasks is always the foreground task.
         List<ActivityManager.RunningTaskInfo> foregroundTaskInfo = am.getRunningTasks(10);
-        int nTasks = foregroundTaskInfo.size();
+        final int nTasks = foregroundTaskInfo.size();
         int[] taskIds = new int[nTasks];
         List<ActivityManager.RunningAppProcessInfo> foregroundAppProcessesInfo = am.getRunningAppProcesses();
         int[] ProcessesIds = new int[nTasks];
-        String[] nombres = new String[nTasks];
-        double[] ram = new double[nTasks];
-        double[] cpu = new double[nTasks];
-        double[] bateria = new double[nTasks];
-        double[] notaMedia = new double[nTasks];
-        double[] nDescargas = new double[nTasks];
-        double[] datosEnv = new double[nTasks];
-        double[] datosRec = new double[nTasks];
-        double[] capacidad = new double[nTasks];
-        double[] r = new double[nTasks];
+        final String[] nombres = new String[nTasks];
+        final double[] ram = new double[nTasks];
+        final double[] cpu = new double[nTasks];
+        final double[] bateria = new double[nTasks];
+        final double[] notaMedia = new double[nTasks];
+        final double[] nDescargas = new double[nTasks];
+        final double[] datosEnv = new double[nTasks];
+        final double[] datosRec = new double[nTasks];
+        final double[] capacidad = new double[nTasks];
+        final double[] r = new double[nTasks];
 
         //PROCESSES
         for (int i = 0; i < nTasks; i++) {
@@ -92,6 +112,15 @@ public class EvaluarActivity extends AppCompatActivity {
             System.out.println(nombres[i]);
         }
 
+
+        //CPU
+
+        for (int i = 0; i < nTasks; i++) {
+            Random random = new Random();
+            cpu[i] = random.nextFloat() * (13.0f - 1.0f) + 1.0f;
+            System.out.println("CPU: " + cpu[i]);
+        }
+
         //RAM
         Debug.MemoryInfo[] memoryInfo = am.getProcessMemoryInfo(ProcessesIds);
         for (int i = 0; i < nTasks; i++) {
@@ -102,17 +131,11 @@ public class EvaluarActivity extends AppCompatActivity {
             System.out.println(memoryInfo[i].otherPss);*/
         }
 
-        //CPU
 
-        for (int i = 0; i < nTasks; i++) {
-            Random random = new Random();
-            cpu[i] = random.nextFloat() * (13.0f - 1.0f) + 1.0f;
-            System.out.println("CPU: " + cpu[i]);
-        }
         //bateria
         for (int i = 0; i < nTasks; i++) {
             Random random = new Random();
-            bateria[i] = random.nextInt(30 - 10) + 10;
+            bateria[i] = random.nextFloat() * (30.0f - 10.0f) + 10.0f;
             System.out.println("bateria: " + bateria[i]);
         }
 
@@ -130,7 +153,7 @@ public class EvaluarActivity extends AppCompatActivity {
         //capacidad
         for (int i = 0; i < nTasks; i++) {
             Random random = new Random();
-            capacidad[i] = random.nextInt(300 - 100) + 100;
+            capacidad[i] = random.nextFloat() * (300.0f - 50.0f) + 50.0f;
             System.out.println("capacidad: " + capacidad[i]);
         }
 
@@ -210,6 +233,19 @@ public class EvaluarActivity extends AppCompatActivity {
 
         //TOMA DE DECISION MULTICRITERIO RIM
 
+        //RANGO DE TRABAJO Y RANGO IDEAL
+        final ResourceData[] recursos = new ResourceData[]{
+                new ResourceData("CPU", 0, 100, 0, 8),
+                new ResourceData("RAM", 0, 1024, 0, 10),
+                new ResourceData("Bateria", 0, 100, 0, 10),
+                new ResourceData("NotaMedia", 0, 5, 4, 5),
+                new ResourceData("NDescargas", 0, 500000000, 100000, 500000000),
+                new ResourceData("DatosEnv", 0, 100, 0, 10),
+                new ResourceData("DatosRec", 0, 100, 0, 10),
+                new ResourceData("Capacidad", 0, 1000, 0, 50)
+        };
+
+        //INICIALIZACIÓN DE LA MATRIZ
         double[][] matriz = new double[8][nTasks];
         matriz[0] = Arrays.copyOf(cpu, cpu.length);
         matriz[1] = Arrays.copyOf(ram, ram.length);
@@ -221,37 +257,9 @@ public class EvaluarActivity extends AppCompatActivity {
         matriz[7] = Arrays.copyOf(capacidad, capacidad.length);
 
 
-        ResourceData[] recursos = new ResourceData[]{
-                new ResourceData("CPU", 0, 100, 0, 8),
-                new ResourceData("RAM", 0, 1024, 0, 10),
-                new ResourceData("Bateria", 0, 100, 0, 10),
-                new ResourceData("NotaMedia", 0, 5, 4, 5),
-                new ResourceData("NDescargas", 0, 500000000, 100000, 500000000),
-                new ResourceData("DatosEnv", 0, 100, 0, 10),
-                new ResourceData("DatosRec", 0, 100, 0, 10),
-                new ResourceData("Capacidad", 0, 1000, 0, 50)
-        };
 
-        /*//[A,B]
-        int[] rangoCPU = new int[] {0, 100};//%n
-        int[] rangoRAM = new int[] {0, 1024};//MB
-        int[] rangoBateria = new int[] {0, 100};
-        int[] rangoNotaMedia = new int[] {0, 5};
-        int[] rangoNDescargas = new int[] {0, 500000000};
-        int[] rangoDatosEnv = new int[] {0, 100};//Kb/s
-        int[] rangoDatosRec = new int[] {0, 100};//Kb/s
-        int[] rangoCapacidad = new int[] {0, 1000};//MB
-        //[C,D]
-        int[] rangoICPU = new int[] {0, 8};
-        int[] rangoIRAM = new int[] {0, 10};
-        int[] rangoIBateria = new int[] {0, 10};
-        int[] rangoINotaMedia = new int[] {4, 5};
-        int[] rangoIDescargas = new int[] {100000, 500000000};
-        int[] rangoIDatosEnv = new int[] {0, 10};
-        int[] rangoIDatosRec = new int[] {0, 10};
-        int[] rangoICapacidad = new int[] {0, 50};
-        */
 
+        //MATRIZ NORMALIZADA Y CON PESOS
         for (int i = 0; i < recursos.length; i++) {
             for (int j = 0; j < nTasks; j++) {
 
@@ -262,6 +270,7 @@ public class EvaluarActivity extends AppCompatActivity {
             }
         }
 
+        //CALCULO DE I- I+ Y R
         for (int j = 0; j < nTasks; j++) {
             double iMinus = 0, iMax = 0;
             for (int i = 0; i < recursos.length; i++) {
@@ -283,37 +292,70 @@ public class EvaluarActivity extends AppCompatActivity {
 
 
         final ListView listview = (ListView) findViewById(R.id.listView);
-        String[] values = new String[nTasks];
+        final ArrayList<String> list = new ArrayList<String>();
+        final ArrayList<String> list2 = new ArrayList<String>(recursos.length);
+        final Dialog dialog = new Dialog(this);
+
+        final String[] values = new String[nTasks];
+        final String[] values2 = new String[] {"","","","","","","",""};
+        list2.addAll(Arrays.asList(values2));
 
         for (int i = 0; i < nTasks; i++) {
-            //values[i]=(nombres[i]+"         "+ ((int)(r[i]*100)));
             values[i]=((int)(r[i]*100)) + "%    " + nombres[i];
         }
 
-        final ArrayList<String> list = new ArrayList<String>();
+
         for (int i = 0; i < values.length; ++i) {
             list.add(values[i]);
         }
+
         final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
         listview.setAdapter(adapter);
+        final ArrayAdapter adapter2 = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list2);
 
-//       //
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, final View view,
-//                                    int position, long id) {
+
+
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view, int position, long id) {
+
+                dialog.setContentView(R.layout.dialog);
+                dialog.setTitle( nombres[position]);
+                ListView listview2 = (ListView) dialog.findViewById(R.id.listView2);
+
+
+
+                values2[0]= "CPU: "+String.valueOf(String.format("%.2f", cpu[position]))+ " %";
+                values2[1]= "RAM: "+String.valueOf(String.format("%.2f",ram[position]))+ " MB";
+                values2[2]= "Bateria: "+String.valueOf(String.format("%.2f",bateria[position])) + " %";
+                values2[3]= "Nota media: "+String.valueOf(String.format("%.2f",notaMedia[position]));
+                values2[4]= "Nº descargas: "+String.valueOf(String.format("%.0f",nDescargas[position]));
+                values2[5]= "Datos Env: "+String.valueOf(String.format("%.2f",datosEnv[position]))+ " kb/s";
+                values2[6]= "Datos Rec: "+String.valueOf(String.format("%.2f",datosRec[position]))+ " kb/s";
+                values2[7]= "Capacidad: "+String.valueOf(String.format("%.2f",capacidad[position]))+ " MB";
+
+
+                for (int i = 0; i < values2.length; ++i) {
+                    list2.set(i,values2[i]);// values 2 esta vacio a rellenar
+                }
+
+                listview2.setAdapter(adapter2);
+
+                dialog.show();
+
 //                final String item = (String) parent.getItemAtPosition(position);
-//                view.animate().setDuration(2000).alpha(0)
-//                        .withEndAction(new Runnable() {
-//                            @Override
-//                            public void run() {
-//                                list.remove(item);
-//                                adapter.notifyDataSetChanged();
-//                                view.setAlpha(1);
-//                            }
-//                        });
-//            }
-//
-//        });
+//                view.animate().setDuration(2000).alpha(0).withEndAction(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        list.remove(item);
+//                        adapter.notifyDataSetChanged();
+//                        view.setAlpha(1);
+//                    }
+//                });
+            }
+        });
     }
 
 //    private class StableArrayAdapter extends ArrayAdapter<String> {
--- app/src/main/java/yonatan24891/effiapp/PreferenciasActivity.java
+++ app/src/main/java/yonatan24891/effiapp/PreferenciasActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class PreferenciasActivity extends AppCompatActivity {
 
@@ -11,6 +14,13 @@ public class PreferenciasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preferencias);
+
+        final ListView listview3 = (ListView) findViewById(R.id.listView3);
+
+        final ArrayList<String> list3 = new ArrayList<String>(8);// get numero de caracteristicas
+
+        //list3.add()
+
     }
 
 
