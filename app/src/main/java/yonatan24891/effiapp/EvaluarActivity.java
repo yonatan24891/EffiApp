c48f26193abc2a214ef1dc5aba827fc37a58db1b
yonatan24891 <yonatan24891@hotmail.com>
2016-09-12 00:32:23 +0200

    Acabada version 1

--- app/src/main/java/yonatan24891/effiapp/EvaluarActivity.java
+++ app/src/main/java/yonatan24891/effiapp/EvaluarActivity.java
@@ -370,40 +370,8 @@ public class EvaluarActivity extends AppCompatActivity {
                     }
                 });
 
-//                final String item = (String) parent.getItemAtPosition(position);
-//                view.animate().setDuration(2000).alpha(0).withEndAction(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        list.remove(item);
-//                        adapter.notifyDataSetChanged();
-//                        view.setAlpha(1);
-//                    }
-//                });
             }
         });
     }
 
-//    private class StableArrayAdapter extends ArrayAdapter<String> {
-//
-//        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
-//
-//        public StableArrayAdapter(Context context, int textViewResourceId,
-//                                  List<String> objects) {
-//            super(context, textViewResourceId, objects);
-//            for (int i = 0; i < objects.size(); ++i) {
-//                mIdMap.put(objects.get(i), i);
-//            }
-//        }
-//
-//        @Override
-//        public long getItemId(int position) {
-//            String item = getItem(position);
-//            return mIdMap.get(item);
-//        }
-//
-//        @Override
-//        public boolean hasStableIds() {
-//            return true;
-//        }
-//    }
 }
